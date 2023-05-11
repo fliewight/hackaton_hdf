8f1af508da846823429d39ebf99e32d7534d8c5d
Julien <lissjulieng@mail.com>
2023-05-11 17:25:41 +0200

    Update main page

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,37 @@
+const URL = 'http://localhost/ville/';
+let one = document.querySelector("#one");
+let two = document.querySelector("#two");
+let three = document.querySelector("#three");
+let four = document.querySelector("#four");
+let five = document.querySelector("#five");
+let six = document.querySelector("#six");
+let seven = document.querySelector("#seven");
+let eight = document.querySelector("#eight");
+one.onclick = function () {
+	document.location.href = URL+"Berck";
+}
+two.onclick = function () {
+	document.location.href = URL+"Beffroi";
+}
+three.onclick = function () {
+	document.location.href = URL+"Bergues";
+}
+four.onclick = function () {
+	document.location.href = URL+"Dunkercques";
+}
+five.onclick = function () {
+	document.location.href = URL+"Lens";
+}
+six.onclick = function () {
+	document.location.href = URL+"Lille";
+}
+seven.onclick = function () {
+	document.location.href = URL+"Cambrai";
+}
+eight.onclick = function () {
+	document.location.href = URL+"Maroilles";
+}
+
 let container = document.querySelector(".container");
 let btn = document.getElementById("spin");
 let number = Math.ceil(Math.random() * 1000);
